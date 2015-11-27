3d37e53d7e5b8bbb0f4418e15e0696dfd0e2e11f
Dreamastiy <editor@xakep.ru>
2015-11-28 00:21:53 +0300

    Loading users id from VK API

--- get_group_users.R
+++ get_group_users.R
@@ -2,8 +2,25 @@ library("RCurl") # Библиотека для генерации запросо
 library("jsonlite") # Библиотека для обработки JSON
 library("stringr") # Библиотека для работы с текстом
 
-# Шаблон запроса подписок аккаунта
-url <- "https://api.vk.com/method/users.getSubscriptions?user_id=" 
+# Шаблон запроса подписчиков сообщества
+url <- "http://api.vk.com/method/groups.getMembers?group_id=dixyclub&fields=sex,bdate,city,country" 
 
 # Шаблон запроса подписчиков аккаунта
 url2 <- "https://api.vk.com/method/users.getFollowers?user_id="
+
+group.count <- fromJSON(resp)$response$count
+
+get_all_users <- function(query, gc) {
+     d <- data.frame()
+     end <- as.integer(gc / 1000)
+     for (i in 1:end){
+          resp <- getURL(paste0(query, "&offset=", i * 1000))
+          Sys.sleep(0.34)
+          d <- rbind(d, fromJSON(resp)$response$users)          
+     }
+     d
+}
+
+l <- get_all_users(url, group.count)
+
+

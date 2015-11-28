694015ccc9ea3104bd06013d6fcbf624590a1803
Dreamastiy <editor@xakep.ru>
2015-11-28 16:37:52 +0300

    VK group graph

--- get_group_users.R
+++ get_group_users.R
@@ -9,8 +9,6 @@ url <- "http://api.vk.com/method/groups.getMembers?group_id=dixyclub&fields=sex,
 # Шаблон запроса друзей аккаунта
 url2 <- "http://api.vk.com/method/friends.get?user_id="
 
-group.count <- fromJSON(resp)$response$count
-
 get_all_users <- function(query, gc) {
      d <- data.frame()
      end <- as.integer(gc / 1000)
@@ -21,6 +19,8 @@ get_all_users <- function(query, gc) {
      }
      d
 }
+
+group.count <- fromJSON(getURL(url))$response$count
 
 l <- get_all_users(url, group.count)
 
@@ -40,16 +40,24 @@ for (x in 1:length(l.sub)){
                 )     
 }
 
+names(d) <- c('uid', 'uid2')
+
+d_uid <- data.frame(uid = unique(d$uid))
+d_names <- l[, c('uid' ,'first_name', 'last_name')]
+test <- merge(d_uid, d_names, by = 'uid', all.x = T)
+test$fullname <- paste0(test$first_name, "_", test$last_name)
+
 vk.network <- graph.data.frame(d, 
                                directed = T)
 
-E(vk.network)$color <- 'black'
+E(vk.network)$color <- hsv(0,0,0,alpha = 0.2)
 E(vk.network)$width <- 1
-V(vk.network)$size <- 5
-V(vk.network)$color <- 'red'
+E(vk.network)$arrow.size <- 0.2
+V(vk.network)$size <- 4
+V(vk.network)$color <- 'pink'
+V(vk.network)$name <- test$fullname
 t <- layout.kamada.kawai(vk.network)
+t <- layout.fruchterman.reingold(vk.network)
 plot(vk.network, 
-     edge.curved = T),
+     edge.curved = T,
      layout = t)
-
-
